2b4eab87e6fd059945184a8ccc2c7323715a7a31
Harry Lawrence <hazbo@gmx.com>
2015-01-04 20:29:52 +0000

    Changed comments according to linter

--- apt.go
+++ apt.go
@@ -25,7 +25,7 @@ type Apt struct {
     Dummy    bool
 }
 
-// Creates a new instance of Apt with some
+// NewApt creates a new instance of Apt with some
 // default values. The action string is the
 // only initial value that is required
 func NewApt(action string) Apt {
@@ -37,7 +37,7 @@ func NewApt(action string) Apt {
     return a
 }
 
-// Generates the command string to be ran based
+// Exec generates the command string to be ran based
 // on apt-get. Currently supports 'update' or
 // 'install'
 func (a Apt) Exec() bool {
@@ -50,12 +50,12 @@ func (a Apt) Exec() bool {
     return true
 }
 
-// Adds a package to install
+// AddPackage adds a package to install
 func (a *Apt) AddPackage(p string) {
     a.Packages = append(a.Packages, p)
 }
 
-// Adds multiple packages to install
+// AddPackages adds multiple packages to install
 func (a *Apt) AddPackages(p []string) {
     for _, pa := range p {
         a.Packages = append(a.Packages, pa)
